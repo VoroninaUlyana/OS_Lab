d1d9169df545ce00f04eb21eb6956082babbef06
Ulyana Voronina <ulyana.s.v.1@gmail.com>
2025-10-07 22:55:13 +0300

    Add Lab2 source files

---
+++ OS_Lab2/OS_Lab2.cpp
@@ -0,0 +1,32 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void parent_mode(const char* exePath);
+void child_mode();
+
+int main(int argc, char* argv[]) 
+{
+    if (argc > 1 && string(argv[1]) == "child") 
+    {
+        cout << "[Child] Started in child mode.\n";
+        child_mode();
+    }
+    else 
+    {
+        cout << "[Parent] Started in parent mode.\n";
+        parent_mode(argv[0]);
+    }
+    return 0;
+}
+
+void parent_mode(const char* exePath) 
+{
+    cout << "[Parent] Parent mode placeholder.\n";
+}
+
+void child_mode() 
+{
+    cout << "[Child] Child mode placeholder.\n";
+}
