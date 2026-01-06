3c9b7c5180e5ac83de02446fac6b6da457a8618c
Ulyana Voronina <ulyana.s.v.1@gmail.com>
2026-01-06 12:10:11 +0300

    Initial implementation of Lab4 before refactoring

---
+++ Browser/Browser.cpp
@@ -0,0 +1,86 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <tchar.h>
+#include <strsafe.h>
+#include <limits>
+using namespace std;
+
+const TCHAR* SEMAPHORE_NAME = _T("DownloadSlots");
+const TCHAR* MUTEX_NAME = _T("LogAccessMutex");
+const TCHAR* EVENT_NAME = _T("BrowserClosingEvent");
+
+int main() 
+{
+    int N, M;
+    cout << "--- Browser Manager ---\n";
+    cout << "Enter max simultaneous downloads (N): ";
+    if (!(cin >> N)) 
+        return 0;
+    cout << "Enter total files in queue (M): ";
+    if (!(cin >> M)) 
+        return 0;
+    cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
+    if (M <= N) 
+    {
+        cout << "Note: M should be greater than N.\n";
+    }
+    if (N <= 0) 
+    {
+        cout << "Note: N should be greater than 0.\n";
+    }
+    HANDLE hSemaphore = CreateSemaphore(NULL, N, N, SEMAPHORE_NAME);
+    HANDLE hMutex = CreateMutex(NULL, FALSE, MUTEX_NAME);
+    HANDLE hExitEvent = CreateEvent(NULL, TRUE, FALSE, EVENT_NAME);
+    if (!hSemaphore || !hMutex || !hExitEvent) 
+    {
+        cerr << "Error: Could not create kernel objects.\n";
+        return 1;
+    }
+    vector<HANDLE> childProcesses;
+    cout << "Starting download processes...\n";
+    for (int i = 0; i < M; ++i) 
+    {
+        STARTUPINFO si;
+        PROCESS_INFORMATION pi;
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(si);
+        ZeroMemory(&pi, sizeof(pi));
+        TCHAR cmdLine[MAX_PATH];
+        StringCchPrintf(cmdLine, MAX_PATH, _T("Downloader.exe file_%d.dat"), i + 1);
+        if (CreateProcess(NULL, cmdLine, NULL, NULL, FALSE,0, NULL, NULL, &si, &pi)) 
+        {
+            childProcesses.push_back(pi.hProcess);
+            CloseHandle(pi.hThread);
+        }
+        else 
+        {
+            cerr << "Error: Failed to launch Downloader.exe\n";
+        }
+    }
+    cout << "\n=============================================\n";
+    cout << "Browser is running.\n";
+    cout << "Active downloads: " << N << ", In queue: " << (M - N) << "\n";
+    cout << "PRESS [ENTER] TO STOP THE BROWSER...\n";
+    cout << "=============================================\n";
+    cin.get();
+    WaitForSingleObject(hMutex, INFINITE);
+    cout << "\n[!] Signal sent! Closing browser...\n";
+    cout << "[!] Waiting for child processes to finish (max 4 seconds)...\n";
+    ReleaseMutex(hMutex);
+    SetEvent(hExitEvent);
+    const int MAX_WAIT = MAXIMUM_WAIT_OBJECTS;
+    for (size_t i = 0; i < childProcesses.size(); i += MAX_WAIT) 
+    {
+        int count = min((int)(childProcesses.size() - i), MAX_WAIT);
+        WaitForMultipleObjects(count, &childProcesses[i], TRUE, INFINITE);
+    }
+    cout << "All processes closed. Press Enter to exit.\n";
+    for (HANDLE h : childProcesses) CloseHandle(h);
+    CloseHandle(hSemaphore);
+    CloseHandle(hMutex);
+    CloseHandle(hExitEvent);
+    cin.get();
+    return 0;
+}
---
+++ Downloader/Downloader.cpp
@@ -0,0 +1,81 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tchar.h>
+#include <ctime>
+using namespace std;
+
+const TCHAR* SEMAPHORE_NAME = _T("DownloadSlots");
+const TCHAR* MUTEX_NAME = _T("LogAccessMutex");
+const TCHAR* EVENT_NAME = _T("BrowserClosingEvent");
+
+struct BracketResult 
+{
+    int openCount;
+    int closeCount;
+};
+
+BracketResult PerformTaskLogic() 
+{
+    string content;
+    for (int i = 0; i < 200; ++i) 
+    {
+        content += "Data block with (some) content [id=" + to_string(i) + "] {check}. ";
+    }
+    int openBrackets = 0;
+    int closeBrackets = 0;
+    for (char c : content) 
+    {
+        if (c == '(' || c == '[' || c == '{') openBrackets++;
+        if (c == ')' || c == ']' || c == '}') closeBrackets++;
+    }
+    return { openBrackets, closeBrackets };
+}
+
+int main(int argc, char* argv[]) 
+{
+    srand(GetTickCount() ^ GetCurrentProcessId());
+    DWORD pid = GetCurrentProcessId();
+    string fileName = "unknown_file.dat";
+    if (argc > 1) 
+    {
+        fileName = argv[1];
+    }
+    HANDLE hSemaphore = OpenSemaphore(SYNCHRONIZE | SEMAPHORE_MODIFY_STATE, FALSE, SEMAPHORE_NAME);
+    HANDLE hMutex = OpenMutex(SYNCHRONIZE, FALSE, MUTEX_NAME);
+    HANDLE hExitEvent = OpenEvent(SYNCHRONIZE, FALSE, EVENT_NAME);
+    if (!hSemaphore || !hMutex || !hExitEvent) 
+    {
+        return 1;
+    }
+    HANDLE waitHandles[2];
+    waitHandles[0] = hExitEvent;
+    waitHandles[1] = hSemaphore;
+    DWORD waitResult = WaitForMultipleObjects(2, waitHandles, FALSE, INFINITE);
+    if (waitResult == WAIT_OBJECT_0) 
+    {
+        WaitForSingleObject(hMutex, INFINITE);
+        cout << "[PID: " << pid << "] Download interrupted by browser closing.\n";
+        ReleaseMutex(hMutex);
+    }
+    else if (waitResult == WAIT_OBJECT_0 + 1) 
+    {
+        WaitForSingleObject(hMutex, INFINITE);
+        cout << "[PID: " << pid << "] Connection established. Starting download of '" << fileName << "'...\n";
+        ReleaseMutex(hMutex);
+        BracketResult result = PerformTaskLogic();
+        int sleepTime = (rand() % 3 + 1) * 1000;
+        Sleep(sleepTime);
+        WaitForSingleObject(hMutex, INFINITE);
+        cout << "[PID: " << pid << "] File '" << fileName << "' processed successfully.\n";
+        cout << "       -> Analysis Result: Open Brackets: " << result.openCount
+            << ", Close Brackets: " << result.closeCount << "\n";
+        ReleaseMutex(hMutex);
+        ReleaseSemaphore(hSemaphore, 1, NULL);
+    }
+    CloseHandle(hSemaphore);
+    CloseHandle(hMutex);
+    CloseHandle(hExitEvent);
+    return 0;
+}
